1b96da845cbee4905ad323231e169124fcedd5dd
cyber-wojtek <wojtek.dudek.pl@gmail.com>
2024-04-22 19:41:42 +0200

    Nope.

--- search/functions_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['none_0',['none',['../class_c_dynamic_bit_set.html#a0b8aa0826a17a317045efad4c1ca193e',1,'CDynamicBitSet::none()'],['../class_c_bit_set.html#ae20f29bf450832f9a6ac14626303087b',1,'CBitSet::none()']]]
-];
--- search/variables_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['data_0',['data',['../struct_bit_set.html#aee214c331b60aed507a95dadb5e293f2',1,'BitSet::data'],['../class_c_dynamic_bit_set.html#a39bc4f1a3655009d7c76233dba7c4c35',1,'CDynamicBitSet::data'],['../class_c_bit_set.html#a1a38814b8d3c85f6b689c2ac3107158e',1,'CBitSet::data']]]
-];
