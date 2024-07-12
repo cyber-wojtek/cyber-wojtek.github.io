205729792c554273df3843aa0758a6ebf34ce175
cyber-wojtek <wojtek.dudek.pl@gmail.com>
2024-07-12 12:26:09 +0200

    Update doc.

--- BitSetCpp/html/classwoj_1_1bitset_1_1reference.js
+++ BitSetCpp/html/classwoj_1_1bitset_1_1reference.js
@@ -4,7 +4,6 @@ var classwoj_1_1bitset_1_1reference =
     [ "reference", "classwoj_1_1bitset_1_1reference.html#a3333e6dcd79f3eea4ce9729e70ff78f1", null ],
     [ "reference", "classwoj_1_1bitset_1_1reference.html#ad72894f615f5f32de51c00e5466ba664", null ],
     [ "~reference", "classwoj_1_1bitset_1_1reference.html#ad4712365f2083e6fd5e91f4ec4de0bc6", null ],
-    [ "clear", "classwoj_1_1bitset_1_1reference.html#abb2fc81abdebe752e1207776ce12a5c1", null ],
     [ "flip", "classwoj_1_1bitset_1_1reference.html#a9e03088e0cf7442193cd17d7f0c418c6", null ],
     [ "operator bool", "classwoj_1_1bitset_1_1reference.html#a070eda3799c0cb199acbfef36530bbbd", null ],
     [ "operator&=", "classwoj_1_1bitset_1_1reference.html#a063ca37d9b3b47529e8bf4e13748d0c2", null ],
@@ -12,6 +11,7 @@ var classwoj_1_1bitset_1_1reference =
     [ "operator=", "classwoj_1_1bitset_1_1reference.html#a48585d830b6dee02086a7e0c48830346", null ],
     [ "operator^=", "classwoj_1_1bitset_1_1reference.html#a93655cf0d4a88d27839d710412b5380f", null ],
     [ "operator|=", "classwoj_1_1bitset_1_1reference.html#a5783637a57ae9565c36970994af2ac3f", null ],
+    [ "reset", "classwoj_1_1bitset_1_1reference.html#ac4d460ab332c65b4a5c11a7460706795", null ],
     [ "set", "classwoj_1_1bitset_1_1reference.html#a88f5230cb3994e4531c8e1f2beb1ac13", null ],
     [ "m_bitset", "classwoj_1_1bitset_1_1reference.html#a25a3e910294c5831b5bdc2927043e354", null ],
     [ "m_index", "classwoj_1_1bitset_1_1reference.html#ac9d8da711751759325d641fd77bd66c3", null ]
